d45ab99bbc60ed896147c30f50352a46abbeddeb
tranletu2003 <tranletu2003@gmail.com>
2025-03-22 17:58:16 +0700

    Obstacles spawning
    Obstacles position  are still bugged

--- _scripts/game.gd
+++ _scripts/game.gd
@@ -1,25 +1,63 @@
 extends Node2D
 
+var coneScene := preload("res://scenes/game/cone.tscn")
+var obstacles : Array
+var lastObs
+
 const cameraPos := Vector2i(579, 324)
 const playerPos := Vector2i(994, 468)
-const groundPos := Vector2i(514, 636)
 
 var speed : float
 const startSpeed : float = 10.0
 const maxSpeed : float = 25.0
+const speedModifier : int = 5000
+
+var groundHeight : int
+var screenSize : Vector2i
+
+var score : int
 
 func _ready() -> void:
+	score = 0
+	screenSize = get_window().size
+	groundHeight = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 
 func new_game():
 	$Player.position = playerPos
 	$Player.velocity = Vector2i(0, 0)
-	$Ground.position = groundPos
+	$Ground.position = Vector2i(0, 0)
 	$Camera2D.position = cameraPos
-	
+
+func generate_obs():
+	if obstacles.is_empty():
+		var obs = coneScene.instantiate()
+		var obsHeight = obs.get_node("Sprite2D").texture.get_height()
+		var obsScale = obs.get_node("Sprite2D").scale
+		var obsX = -screenSize.x - score - 100
+		var obsY = screenSize.y - groundHeight - (obsHeight * obsScale.y / 2)
+		lastObs = obs
+		obs.position = Vector2i(obsX, obsY)
+		add_child(obs)
+		obstacles.append(obs)
+
+func _process(delta: float) -> void:
+	score += speed
+	print(score)
+	generate_obs()
+	for obs in obstacles:
+		if obs.position.x > ($Camera2D.position.x + screenSize.x):
+				remove_obs(obs)
+
 func _physics_process(delta: float) -> void:
-	speed = startSpeed
+	speed = startSpeed + score / speedModifier
+	if speed > maxSpeed:
+		speed = maxSpeed
 	
 	$Player.position.x -= speed
 	$Camera2D.position.x -= speed
 	$Ground.position.x -= speed
+
+func remove_obs(obs):
+	obs.queue_free()
+	obstacles.erase(obs)
